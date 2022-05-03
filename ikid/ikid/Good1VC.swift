88f9d1d817854351a6b92809d4443b5861f343ec
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2022-05-02 19:45:15 -0700

    got first joke to work

---
+++ ikid/ikid/Good1VC.swift
@@ -0,0 +1,30 @@
+//
+//  Good1VC.swift
+//  ikid
+//
+//  Created by Ryan Oh on 5/2/22.
+//
+
+import UIKit
+
+class Good1VC: UIViewController {
+
+    @IBOutlet weak var goodJoke: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        goodJoke.text = "What kind of tea \nis hard to swallow?"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ikid/ikid/GoodVC.swift
+++ ikid/ikid/GoodVC.swift
@@ -9,22 +9,66 @@ import UIKit
 
 class GoodVC: UIViewController {
 
-    @IBOutlet weak var goodJoke: UILabel!
+    var firstViewController : Good1VC! = nil
+    var secondViewController : Good2VC! = nil
+    @IBAction func switchViews(_ sender: Any) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            secondViewController.view.frame = self.view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        }
+        else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+        UIView.commitAnimations()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        goodJoke.text = "What kind of tea \nis hard to swallow?"
+        firstBuilder()
+        switchViewController(nil, to: firstViewController)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParent: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParent()
+        }
+        
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
     }
-    */
+
+    fileprivate func firstBuilder() {
+        if firstViewController == nil {
+            firstViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "First")
+            as! Good1VC
+        }
+    }
+    
+    fileprivate func secondBuilder() {
+        if secondViewController == nil {
+            secondViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "Second")
+            as! Good2VC
+        }
+    }
 
 }
